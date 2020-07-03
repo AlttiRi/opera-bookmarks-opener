718f3549c4eb1b40e535cf01082f215191252c1c
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-07-03 09:28:49 +0300

    Add _dev-notes.txt

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
-//todo open in bookmarks tab
-
 chrome.browserAction.onClicked.addListener(function(tab) {
-
-    chrome.tabs.create({url: "opera://startpage/bookmarks"});  
-	
-	// if
-	//chrome.tabs.update(tab.id, {url: "opera://startpage/bookmarks"});
-
+    chrome.tabs.create({url: "opera://startpage/bookmarks"});
 });
